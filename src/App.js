9a43e049082c3b11befbc3990c3eff1c6df4c9d3
Hyun <dacapo629@naver.com>
2025-09-10 11:46:47 +0900

    git 테스트용으로 수정

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      테스트용으로 기본 화면 지워봤음.
     </div>
   );
 }
